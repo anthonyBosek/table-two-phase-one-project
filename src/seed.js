ae20e5117d48d1578fb5a74020d669523f00b50f
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-25 11:35:04 -0700

    seed inventory with updated card info

--- src/seed.js
+++ src/seed.js
@@ -1,1 +1,59 @@
-console.log('demo 000', getJSON);
+const pokemonUrl = "http://localhost:3000/pokemon?_limit=50";
+const postPokemonUrl = "http://localhost:3000/inventory"
+const weaknessArray = ["Bug", "Poison", "Fire", "Psychic", "Flying", "Ice",  "Water", "Ground", "Rock", "Electric"];
+
+const postPokemon = (pokeObj) => {
+    fetch(postPokemonUrl, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(pokeObj)
+    })
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw (response.statusText);
+        }
+    })
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => alert(err))
+}
+
+const updatePokemon = (pokeObj) => {
+    const newObj = {...pokeObj};
+    newObj.inventory = Math.floor(Math.random() * 21);
+    newObj.price = `$${Math.floor(Math.random() * 101).toFixed(2)}`;
+    newObj.resistance = weaknessArray[Math.floor(Math.random() * weaknessArray.length)];
+    newObj.resistanceAmount = Math.floor((Math.random() * 5) + 1) * 10;
+    newObj.retreatCost = Math.floor(Math.random() * 4);
+    newObj.hitPoints = Math.floor(Math.random() * 100) + 20;
+    newObj.powerText = "This is just some random generic text intended to fill up space on the card while it displays on the page. Hopefully, no one will actually read this too closely, but if they do maybe they will not comment on it."
+    newObj.abilitiesText = "Much shorter random text to fill up card space.";
+    newObj.abilityStrength = (Math.floor(Math.random() * 10) + 1) * 10;
+    newObj.abilityIconAmount = Math.floor(Math.random() * 4) + 1;
+    newObj.flavorText = "This project was a blast and my teammates are awesome!";
+    newObj.weakness = weaknessArray[Math.floor(Math.random() * weaknessArray.length)];
+    postPokemon(newObj);
+}
+
+const getAllPokemon = () => {
+    fetch(pokemonUrl)
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw (response.statusText);
+        }
+    })
+    .then(data => {
+        data = data.slice(25);
+        data.forEach(item => updatePokemon(item));
+    })
+    .catch(err => alert(err))
+}
+
+getAllPokemon();
