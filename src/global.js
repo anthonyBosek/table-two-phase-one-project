93830f710588fbe79628334f9a36a67498694af7
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-25 13:56:53 -0700

    save point

--- src/global.js
+++ src/global.js
@@ -1,8 +1,28 @@
 // Global Variables
 const tableContainer = document.getElementById("table-container");
+const modalContainer = document.getElementById("modal-container");
+const modalClose = document.getElementById("modal-close");
+const modal = document.getElementById("modal");
 const search = document.getElementById("search");
 const mode = document.getElementById("mode");
 const cart = document.getElementById("cart");
-const modalContainer = document.getElementById("modal-container");
-const modal = document.getElementById("modal");
-const modalClose = document.getElementById("modal-close");
+
+// temp data vars
+const userCartData = {
+  customerNumber: 0,
+  name: "",
+  amountDue: 0,
+  items: [],
+};
+
+// const nameSearchData = {
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+//   pokeName: "pokeId",
+// };
