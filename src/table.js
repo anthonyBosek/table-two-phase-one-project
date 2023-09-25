5285d197b566e26f3adc53600b8925aa382258d5
Austin Ohlfs <ohlfsam96@gmail.com>
2023-09-25 15:00:14 -0600

    table-first-draft

--- src/table.js
+++ src/table.js
@@ -32,19 +32,29 @@ const displayPokemonTable = (pokemonArray) => {
     const cardPrice = document.createElement('td')
     
     pokemonImg.src = pokemon.ThumbnailImage
+    pokemonImg.alt = pokemon.name
     pokemonName.textContent = pokemon.name
     pokemonIndex.textContent = pokemon.number
     pokemonType.textContent = pokemon.type
-    pokemonImg.src = pokemon.ThumbnailImage
     cardQty.textContent = '0'
     cardPrice.textContent = '$0.00'
-    if (pokemon.type[0] === 'grass') {
+    if (pokemon.type[0] === 'grass' ) {
       pokemonItem.classList.add('table-success')
-    }
+    } else if (pokemon.type[0] === 'fire' || pokemon.type[0] === 'dragon') {
+      pokemonItem.classList.add('table-danger')
+    } else if (pokemon.type[0] === 'water' || pokemon.type[0] === 'ice') {
+      pokemonItem.classList.add('table-info')
+    } else if (pokemon.type[0] === 'electric') {
+      pokemonItem.classList.add('table-warning')
+    } else if (pokemon.type[0] === 'poison') {
+      pokemonItem.classList.add('table-primary')
+    } else if (pokemon.type[0] === 'ground' || pokemon.type[0] === 'rock') {
+      pokemonItem.classList.add('table-secondary')
+    } 
     pokemonImgData.append(pokemonImg)
     pokemonItem.append(pokemonIndex, pokemonImgData, pokemonName, pokemonType, cardQty, cardPrice)
     pokemonTable.append(pokemonItem)
-    
+
   })
  
 }
