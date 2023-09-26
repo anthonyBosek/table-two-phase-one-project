e2cdfe84dd55ad69fc6e47a648835b6bc7e70d6a
Brian Richie Sr <brianrichiesr@gmail.com>
2023-09-26 09:01:31 -0700

    add some modal functionality

--- src/global.js
+++ src/global.js
@@ -3,7 +3,16 @@ const URL = "http://localhost:3000/inventory";
 const USERS_URL = "http://localhost:3000/users";
 const POKEMON_URL = "http://localhost:3000/pokemon";
 // Table Variables
+// Table Variables
 const pokemonTable = document.querySelector("#table-body");
+const pokemonItem = document.createElement("tr");
+const pokemonName = document.createElement("td");
+const pokemonImgData = document.createElement("td");
+const pokemonImg = document.createElement("img");
+const pokemonIndex = document.createElement("td");
+const pokemonType = document.createElement("td");
+const cardQty = document.createElement("td");
+const cardPrice = document.createElement("td");
 // Nav-Bar Variables
 const tableContainer = document.getElementById("table-container");
 const modalContainer = document.getElementById("modal-container");
@@ -15,12 +24,6 @@ const dropdownContent = document.getElementById("dropdown-content");
 const mode = document.getElementById("mode");
 const cart = document.getElementById("cart");
 // Modal Variables
-const h2 = document.createElement("h2");
-const nameH4 = document.createElement("h4");
-const table = document.createElement("table");
-const amtH4 = document.createElement("h4");
-const numItemsH4 = document.createElement("h4");
-
 // temp data vars
 const userCartData = {
   customerNumber: 10011001,
@@ -33,35 +36,86 @@ const userCartData = {
   },
 };
 
-// Modal Cart
-const createTableRowData = () => {};
+const h2 = document.createElement("h2");
+const span1 = document.createElement("span");
+h2.textContent = `Customer No.: `;
+h2.append(span1);
+const hr1 = document.createElement("hr");
+const span2 = document.createElement("span");
+const nameH4 = document.createElement("h4");
+nameH4.textContent = `Customer Name: `;
+nameH4.append(span2);
+const numItemsH4 = document.createElement("h4");
+const hr2 = document.createElement("hr");
+const amtH4 = document.createElement("h4");
+const span3 = document.createElement("span");
+amtH4.textContent = `Amount Due: `
+amtH4.append(span3);
+const hr3 = document.createElement("hr");
+const table = document.createElement("table")
+const tableHead = document.createElement("thead");
+tableHead.innerHTML = `
+  <tr>
+    <th class="right-border width-15-pc">Qty</th>
+    <th class="right-border width-50-pc">Name</th>
+    <th class=" width-15-pc">Cost (Each)</th>
+    <th class="left-border  width-15-pc">Cost (Total)</th>
+  </tr>
+`
+const tableBody = document.createElement("tbody");
+table.append(tableHead, tableBody);
+table.classList.add("width-100-pc");
+modal.append(h2, hr1, nameH4, hr2, amtH4, hr3, table);
+let totalPrice = 0;
+let quantity = 0;
 
-const createTable = () => {
-  const tableHead = document.createElement("thead");
-  tableHead.innerHTML = `
-    <tr>
-      <th class="right-border">Qty</th>
-      <th class="right-border">Name</th>
-      <th>Cost (Each)</th>
-      <th class="left-border">Cost (Total)</th>
-    </tr>
-  `;
-  table.append(tableHead);
-  return table;
-};
+
+// Modal Cart
+const getPokemon = (pokeId, qty) => {
+
+  fetch(`${URL}/${pokeId}`)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw (response.statusText);
+    }
+  })
+  .then(data => {
+    createTableRowData(data, qty);
+  })
+  .catch(err => alert(err))
+
+}
+
+const createTableRowData = (pokeObj, amt) => {
+  console.log(pokeObj, amt);
+  const tr = document.createElement("tr");
+  let total = (pokeObj.price * amt).toFixed(2)
+  quantity += 1;
+  totalPrice += Number(total);
+  tr.innerHTML = `
+    <td class="right-border width-15-pc">${amt}</td>
+    <td class="right-border width-50-pc">${pokeObj.name}</td>
+    <td class=" width-15-pc">$${pokeObj.price}</td>
+    <td class="left-border  width-15-pc">$${total}</td>
+`
+tableBody.append(tr);
+span3.textContent = `$ ${totalPrice.toFixed(2)}`;
+}
 
 const displayCartData = () => {
-  console.log("cart", userCartData);
-  h2.textContent = `Customer No.: ${userCartData.customerNumber} `;
-  const hr1 = document.createElement("hr");
-  const nameH4 = document.createElement("h4");
-  nameH4.textContent = `Customer Name: ${userCartData.name}`;
-  const hr2 = document.createElement("hr");
-  amtH4.textContent = `Amount Due: $${userCartData.amountDue}`;
-  const hr3 = document.createElement("hr");
-  const itemsTable = createTable();
-  modal.append(h2, hr1, nameH4, hr2, amtH4, hr3, itemsTable);
-};
+  
+  span1.textContent = userCartData.customerNumber;
+  span2.textContent = userCartData.name
+  tableBody.innerHTML = "";
+  totalPrice = 0;
+  quantity = 0;
+  for (let each in userCartData.items) {
+    getPokemon(`${searchObj[each]}`, userCartData.items[each])
+  }
+  
+}
 
 // Search Bar
 const searchObj = {};
--- src/table.js
+++ src/table.js
@@ -1,56 +1,15 @@
-const displayPokemonTable = (pokemonArray) => {
-  pokemonArray.forEach((pokemon) => {
-    const pokemonItem = document.createElement("tr");
-    const pokemonName = document.createElement("td");
-    const pokemonImgData = document.createElement("td");
-    const pokemonImg = document.createElement("img");
-    const pokemonIndex = document.createElement("td");
-    const pokemonType = document.createElement("td");
-    const cardQty = document.createElement("td");
-    const cardPrice = document.createElement("td");
-
-    pokemonImg.src = pokemon.ThumbnailImage;
-    pokemonImg.alt = pokemon.name;
-    pokemonName.textContent = pokemon.name;
-    pokemonIndex.textContent = pokemon.number;
-    pokemonType.textContent = pokemon.type.join(", ");
-    cardQty.textContent = "0";
-    cardPrice.textContent = "$0.00";
-
-    if (pokemon.type[0] === "grass") {
-      pokemonItem.classList.add("table-success");
-    } else if (pokemon.type[0] === "fire" || pokemon.type[0] === "dragon") {
-      pokemonItem.classList.add("table-danger");
-    } else if (pokemon.type[0] === "water" || pokemon.type[0] === "ice") {
-      pokemonItem.classList.add("table-info");
-    } else if (pokemon.type[0] === "electric" || pokemon.type[0] === "normal") {
-      pokemonItem.classList.add("table-warning");
-    } else if (pokemon.type[0] === "poison" || pokemon.type[0] === "bug") {
-      pokemonItem.classList.add("table-primary");
-    } else if (pokemon.type[0] === "ground" || pokemon.type[0] === "rock") {
-      pokemonItem.classList.add("table-secondary");
-    }
-
-    // hover effect
-    pokemonItem.addEventListener("mouseenter", (event) => {
-      pokemonItem.classList.add("scale");
-      pokemonImg.classList.add("img-scale");
-    });
-    pokemonItem.addEventListener("mouseleave", (event) => {
-      pokemonItem.classList.remove("scale");
-      pokemonImg.classList.remove("img-scale");
-    });
-  }
-});
-
+//Global vairables
+const selectPriceFilter = document.querySelector('#price-filter')
+const selectQtyFilter = document.querySelector('#qty-filter')
+const selectNameFilter = document.querySelector('#name-filter')
+const filterByNumber = document.querySelector('#number')
+const selectTypeFilter = document.querySelector('#type-filter')
 // --------------------------------------------------------------------------------------------
 
-
-
 const displayPokemonTable = (pokemonArray) => {
-    pokemonTable.innerHTML = ''
-    pokemonArray.forEach(pokemon => {
-
+  pokemonTable.innerHTML = ''
+  pokemonArray.forEach(pokemon => {
+    
     const pokemonItem = document.createElement('tr')
     const pokemonName = document.createElement('td') 
     const pokemonImgData = document.createElement('td')
@@ -67,7 +26,7 @@ const displayPokemonTable = (pokemonArray) => {
     pokemonType.textContent = pokemon.type[0]
     cardQty.textContent = pokemon.inventory
     cardPrice.textContent = `$${pokemon.price}`
-
+    
     if (pokemon.type[0] === 'grass' ) {
       pokemonItem.classList.add('table-success')
     } else if (pokemon.type[0] === 'fire' || pokemon.type[0] === 'dragon') {
@@ -81,22 +40,25 @@ const displayPokemonTable = (pokemonArray) => {
     } else if (pokemon.type[0] === 'ground' || pokemon.type[0] === 'rock') {
       pokemonItem.classList.add('table-secondary')
     } 
-
+    // hover effect
+    pokemonItem.addEventListener("mouseenter", (event) => {
+      pokemonItem.classList.add("scale");
+      pokemonImg.classList.add("img-scale");
+    });
+    pokemonItem.addEventListener("mouseleave", (event) => {
+      pokemonItem.classList.remove("scale");
+      pokemonImg.classList.remove("img-scale");
+    });
+    
     pokemonImgData.append(pokemonImg)
     pokemonItem.append(pokemonIndex, pokemonImgData, pokemonName, pokemonType, cardQty, cardPrice)
     pokemonTable.append(pokemonItem)
     
   })
- 
+  
 }
 
-//Global vairables
-const selectPriceFilter = document.querySelector('#price-filter')
-const selectQtyFilter = document.querySelector('#qty-filter')
-const selectNameFilter = document.querySelector('#name-filter')
-const filterByNumber = document.querySelector('#number')
-const selectTypeFilter = document.querySelector('#type-filter')
-
+// --------------------------------------------------------------------------------------------
 
 //eventLiseners
 filterByNumber.addEventListener('click', () => {
