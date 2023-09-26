a0c7fe34bf5c12f9f3617aa5346b4f93c22c6a98
Anthony Bosek <anthony.bosek@gmail.com>
2023-09-26 11:01:00 -0700

    card component wireframe/layout

---
+++ src/card.js
@@ -0,0 +1,19 @@
+// <div id="card">
+// <div>
+//   <span id="card-title">Pikachu</span>
+//   <span id="card-subtitle">120 HP</span>
+// </div>
+// <div id="card-img">
+//   <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png" alt="test">
+// </div>
+// <div id="card-vitals">
+//   <span>Height: 6-1</span>
+//   <span>Weight: 200</span>
+// </div>
+// <p id="card-power">This is just some random generic text intended to fill up space on the card while it displays on the page. Hopefully, no one will actually read this too closely, but if they do maybe they will not comment on it.</p>
+// <hr>
+// <p id="card-ability">Much shorter random text to fill up card space.</p>
+// <hr>
+// </div>
+
+const createCard = (pokemonObj) => {};
